ab106af2cb9ffeac2eb178ae4da952c4044b9647
sailerproXD <emmanuel.agredo@udea.edu.co>
2026-08-16 17:33:01 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+# Ejercicio que pide ingresar un numero obteniendo, el cuadrado y el cubo del mismo
+a = int(input("ingrese un numero"))
+c2=a**2
+c3=a**3
+print("el cuadrado del numero es:{x}\n el cubo del numero es:{y}".format(x=c2, y=c3))
+    
