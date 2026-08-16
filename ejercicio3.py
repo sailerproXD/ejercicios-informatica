53676ed9c351200901d7a169b9f4d0edc38b14a6
sailerproXD <emmanuel.agredo@udea.edu.co>
2026-08-16 18:16:53 -0500

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+# ejercicio 3 donde tengo que promediar las 5 notas de un curso 
+n1= float(input("ingrese la primera nota:"))
+n2= float(input("ingrese la segunda nota:"))
+n3= float(input("ingrese la tercera nota:"))
+n4= float(input("ingrese la cuarta nota:"))
+n5= float(input("ingrese la quinta nota:"))
+Nota = (n1*0.30) + (n2*0.15) + (n3*0.15) + (n4*0.20) + (n5*0.20)
+print( Nota ) 
