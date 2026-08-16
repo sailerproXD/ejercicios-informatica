2a377d539c472518fea6fae5f03d703a4f9c5814
sailerproXD <emmanuel.agredo@udea.edu.co>
2026-08-16 17:45:53 -0500

    Add files via upload

---
+++ main ejercicio 2.py
@@ -0,0 +1,8 @@
+#Ejercicio 2, sueldo de trabajadores con porcentajes de aumento
+a = float(input("ingrese el sueldo del trabajador:"))
+Trab1 = a+a*0.1
+Trab2 = a+a*0.12
+Trab3 = a+a*0.15
+print("el sueldo del trabajador 1 es{x}".format(x=Trab1))
+print("el sueldo del trabajador 2 es{y}".format(y=Trab2))
+print("el sueldo del trabajador 3 es{z}".format(z=Trab3))
